157c865a20aad9bf4240cd3d1e28457c59182104
らいか <raika4789@gmail.com>
2022-09-23 13:34:03 +0900

    add Makefile

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = so_long
+CLIENT_SRCS = clients.c
+SERVER_SRCS = server.c
+SRCS = utils.c
+CC = gcc
+CFLAGS = -Wall -Werror -Wextra
+
+.PHONY : all clean fclean re
+
+all: $(NAME)
+
+$(NAME): $(SRCS)
+	make -C libft
+	cp ./libft/libft.a .
+	$(CC) $(CFLAGS) $(CLIENT_SRCS) $(SRCS) -L. -lft -o client
+	$(CC) $(CFLAGS) $(SERVER_SRCS) $(SRCS) -L. -lft -o server
+
+clean:
+	make -C libft clean
+	rm -f $(OBJS)
+
+fclean: clean
+	make -C libft fclean
+	rm -f libft.a
+	rm -f $(NAME)
+
+re: fclean all
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: takanoraika <takanoraika@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 23:51:12 by takanoraika       #+#    #+#             */
-/*   Updated: 2022/09/23 13:10:48 by takanoraika      ###   ########.fr       */
+/*   Updated: 2022/09/23 13:31:44 by takanoraika      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include <stdio.h>
 static void Signal1(int code)
 {
+	(void)code;
 	printf("0\n");
 	signal(SIGUSR1, Signal1);
 }
 
 static void Signal2(int code)
 {
+	(void)code;
 	printf("1\n");
 	signal(SIGUSR2, Signal2);
 }
